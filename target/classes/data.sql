af54a30a2e62c5e7cf1d8d5d304ce8b6e9c58d6b
Henry Leung <henryleunghc@gmail.com>
2024-11-24 02:33:27 +0800

    add

--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-MERGE INTO users (username, password, enabled) KEY (username) VALUES ('admin', 'admin', true);
-MERGE INTO authorities (username, authority) KEY (username, authority) VALUES ('admin', 'ROLE_USER');
