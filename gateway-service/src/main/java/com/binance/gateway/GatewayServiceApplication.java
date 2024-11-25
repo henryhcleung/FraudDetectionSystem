0c1653a20f92a25001f04f60ef0369715734f49e
Henry Leung <henryleunghc@gmail.com>
2024-11-25 19:24:48 +0800

    add

---
+++ gateway-service/src/main/java/com/binance/gateway/GatewayServiceApplication.java
@@ -0,0 +1,22 @@
+package com.binance.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("fraud-detection-service", r -> r.path("/fraud/**")
+                        .uri("lb://fraud-detection-service"))
+                .build();
+    }
+}
