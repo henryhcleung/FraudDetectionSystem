33a9748dcba62b1d14286666d556bce5457c0611
Henry Leung <henryleunghc@gmail.com>
2024-11-27 02:38:53 +0800

    add

--- fraud-detection-service/src/main/resources/schema.sql
+++ fraud-detection-service/src/main/resources/schema.sql
@@ -1,14 +1,17 @@
+-- Create the transactions table if it does not exist
 CREATE TABLE IF NOT EXISTS TRANSACTIONS (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     details VARCHAR(255)
 );
 
+-- Create the users table if it does not exist
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL PRIMARY KEY,
-    password VARCHAR(100) NOT NULL,
+    password VARCHAR(100000) NOT NULL,
     enabled BOOLEAN NOT NULL
 );
 
+-- Create the authorities table if it does not exist
 CREATE TABLE IF NOT EXISTS authorities (
     username VARCHAR(50) NOT NULL,
     authority VARCHAR(50) NOT NULL,
