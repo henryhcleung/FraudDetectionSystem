35bf2fcb90d66983bbc111288c236c3cd4181696
Henry Leung <henryleunghc@gmail.com>
2024-11-27 03:31:44 +0800

    add

--- fraud-detection-service/src/main/resources/schema.sql
+++ fraud-detection-service/src/main/resources/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS TRANSACTIONS (
 -- Create the users table if it does not exist
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL PRIMARY KEY,
-    password VARCHAR(100000) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     enabled BOOLEAN NOT NULL
 );
 
