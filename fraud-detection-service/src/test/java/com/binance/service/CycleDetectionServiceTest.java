a311be69c3569b103f9dcf15cf49b359d8bf5403
Henry Leung <henryleunghc@gmail.com>
2024-11-26 23:27:20 +0800

    add

--- fraud-detection-service/src/test/java/com/binance/service/CycleDetectionServiceTest.java
+++ fraud-detection-service/src/test/java/com/binance/service/CycleDetectionServiceTest.java
@@ -5,10 +5,12 @@ import com.binance.model.ListNode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = FraudDetectionServiceApplication.class)
+@ActiveProfiles("test")
 public class CycleDetectionServiceTest {
 
     @Autowired
